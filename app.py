71c08ed5de181501510e4b6eaf9b755d7a59139d
gmichail147 <giorgosmichail147@gmail.com>
2022-03-16 10:47:50 +0000

    sensor values shown on main page

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask, render_template
+#!/usr/bin/python
 
-#hello from windows
+from flask import Flask, jsonify, redirect, render_template
+from bme280 import BME280
+
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus) # BME280 temperature, humidity and pressure sensor
+
+def get_sensor_data():
+	data = {}
+	data['humidity'] = bme280.get_humidity()
+	data['pressure'] = bme280.get_pressure()
+	data['temperature'] = bme280.get_temperature()
+	return data
+
 
 app = Flask(__name__)
 
-@app.route("/")
-
+@app.route('/')
 def index():
-    return render_template("index.html")
+	data = get_sensor_data()
+	return render_template('index.html', data=sorted(data.items()))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+@app.route('/download')
+def download():
+	return jsonify(get_sensor_data())
+
+
+
+
+if __name__ == '__main__':
+	
+	app.run(debug=False, host='0.0.0.0')
+
